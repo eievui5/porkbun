6f64077cb521cad4885993254c856221301032fc
Evie <14899090+eievui5@users.noreply.github.com>
2024-08-14 09:16:55 -0400

    ipv6 support

--- ddns/src/main.rs
+++ ddns/src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use std::net::Ipv4Addr;
 use std::path::PathBuf;
 use std::process::exit;
 use tracing::{error, info};
@@ -15,6 +14,14 @@ struct Cli {
     #[clap(short, long)]
     silent: bool,
 
+    /// Update ipv4 address.
+    #[clap(short = '4', long)]
+    ipv4: bool,
+
+    /// Update ipv6 address.
+    #[clap(short = '6', long)]
+    ipv6: bool,
+
     /// Which subdomain to update, if any.
     // 'w' is for www, meaning you can do `ddns -wwww example.com`
     #[clap(short = 'w', long)]
@@ -25,80 +32,151 @@ struct Cli {
     domain: String,
 }
 
-fn matches_previous_ipv4(
-    client: &porkbun::Client,
-    config: &Cli,
-    address: &Ipv4Addr,
-) -> porkbun::Result<Option<bool>> {
-    let ipv4_records = client.fetch_ipv4_records(&config.domain, config.subdomain.as_deref())?;
-
-    let name = config
-        .subdomain
-        .as_deref()
-        .unwrap_or(config.domain.as_ref());
-
-    if let Some(previous_ip) = ipv4_records.iter().find(|x| x.name == name) {
-        if previous_ip.address == *address {
-            Ok(Some(true))
-        } else {
-            Ok(Some(false))
-        }
-    } else {
-        Ok(None)
-    }
-}
-
 fn main() {
     let config = Cli::parse();
     tracing_subscriber::fmt::init();
 
-    // TODO: Config (keyfile path + domain name)
     let client = porkbun::Client::open_keys(&config.key).unwrap_or_else(|msg| {
-        error!("failed to open key file: {msg}");
+        error!("failed to open key file ({}): {msg}", config.key.display());
         exit(1);
     });
-    let Some(ip_address) = client.ping_ipv4().unwrap_or_else(|msg| {
-        error!("failed to retreive public ipv4 address: {msg}");
-        exit(1);
-    }) else {
-        error!("ip address is not present; quitting");
-        exit(1);
+
+    let record_name = config
+        .subdomain
+        .as_deref()
+        .unwrap_or(config.domain.as_ref());
+
+    let mut error_count = 0;
+
+    if config.ipv4 && !update_ipv4(&client, &config, record_name) {
+        error_count += 1;
+    }
+
+    if config.ipv6 && !update_ipv6(&client, &config, record_name) {
+        error_count += 1;
+    }
+
+    exit(error_count);
+}
+
+fn update_ipv6(client: &porkbun::Client, config: &Cli, record_name: &str) -> bool {
+    let ip_address = match client.ping_ipv6() {
+        Ok(Some(address)) => address,
+        Ok(None) => {
+            error!("ipv6 address is not present");
+            return false;
+        }
+        Err(msg) => {
+            error!("failed to retreive public ipv6 address: {msg}");
+            return false;
+        }
     };
 
-    match matches_previous_ipv4(&client, &config, &ip_address) {
+    match client
+        .fetch_ipv6_records(&config.domain, config.subdomain.as_deref())
+        .map(|records| {
+            records
+                .iter()
+                .find(|x| x.name == record_name)
+                .map(|x| x.address == ip_address)
+        }) {
+        Ok(Some(true)) => {
+            if !config.silent {
+                info!("current ipv6 record matches public ip address");
+            }
+            true
+        }
+        Ok(Some(false)) => {
+            if let Err(msg) =
+                client.edit_ipv6_address(&config.domain, config.subdomain.as_deref(), &ip_address)
+            {
+                error!("failed to edit ipv6 address: {msg}");
+                return false;
+            } else if !config.silent {
+                info!("successfully updated ipv6 record to {ip_address}");
+            }
+            true
+        }
+        Ok(None) => {
+            if let Err(msg) = client.create_record(
+                &config.domain,
+                config.subdomain.as_deref(),
+                porkbun::RecordType::Aaaa,
+                &ip_address.to_string(),
+                None,
+                None,
+            ) {
+                error!("failed to create ipv6 record: {msg}");
+                return false;
+            } else if !config.silent {
+                info!("successfully created ipv6 record: {ip_address}");
+            }
+            true
+        }
+        Err(msg) => {
+            error!("failed to retrieve previous ipv6 address: {msg}");
+            false
+        }
+    }
+}
+
+fn update_ipv4(client: &porkbun::Client, config: &Cli, record_name: &str) -> bool {
+    let ip_address = match client.ping_ipv4() {
+        Ok(Some(address)) => address,
+        Ok(None) => {
+            error!("ipv4 address is not present");
+            return false;
+        }
+        Err(msg) => {
+            error!("failed to retreive public ipv4 address: {msg}");
+            return false;
+        }
+    };
+
+    match client
+        .fetch_ipv4_records(&config.domain, config.subdomain.as_deref())
+        .map(|records| {
+            records
+                .iter()
+                .find(|x| x.name == record_name)
+                .map(|x| x.address == ip_address)
+        }) {
         Ok(Some(true)) => {
             if !config.silent {
                 info!("current ipv4 record matches public ip address");
             }
-            exit(0);
+            true
         }
         Ok(Some(false)) => {
             if let Err(msg) =
                 client.edit_ipv4_address(&config.domain, config.subdomain.as_deref(), &ip_address)
             {
                 error!("failed to edit ipv4 address: {msg}");
-                exit(1);
+                return false;
             } else if !config.silent {
                 info!("successfully updated ipv4 record to {ip_address}");
             }
+            true
         }
         Ok(None) => {
             if let Err(msg) = client.create_record(
                 &config.domain,
                 config.subdomain.as_deref(),
-                porkbun::RecordType::A,
+                porkbun::RecordType::Aaaa,
                 &ip_address.to_string(),
                 None,
                 None,
             ) {
                 error!("failed to create ipv4 record: {msg}");
-                exit(1);
+                return false;
             } else if !config.silent {
                 info!("successfully created ipv4 record: {ip_address}");
             }
+            true
         }
         Err(msg) => {
-            error!("failed to retrieve previous ip address: {msg}");
+            error!("failed to retrieve previous ipv4 address: {msg}");
+            false
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![warn(clippy::unwrap_used)]
 
-use std::net::Ipv4Addr;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 #[derive(Clone, Debug)]
 pub struct Client {
@@ -20,11 +20,18 @@ pub enum Error {
     Json(#[from] serde_json::Error),
     #[error("porkbun API returned an error: \"{message}\"")]
     Api { message: String },
+    #[error("porkbun API returned an unrecognized response ({response})")]
+    MalformedApi { response: String },
     #[error("porkbun API returned an unrecognized response ({response}): {error}")]
-    MalformedApi {
+    MalformedApiSerde {
         error: serde_json::Error,
         response: String,
     },
+
+    #[error("porkbun API returned an ipv4 address ({0}) when an ipv6 was expected")]
+    UnexpectedIpv4(Ipv4Addr),
+    #[error("porkbun API returned an ipv6 address ({0}) when an ipv4 was expected")]
+    UnexpectedIpv6(Ipv6Addr),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -77,16 +84,23 @@ pub struct DnsRecord {
     pub notes: Option<String>,
 }
 
-#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
-pub struct Ipv4Record {
-    pub id: String,
-    pub name: String,
-    #[serde(rename = "content")]
-    pub address: Ipv4Addr,
-    pub ttl: String,
-    pub prio: String,
-    pub notes: Option<String>,
+macro_rules! typed_record {
+    ($name:ident, $field:ident, $type:ty) => {
+        #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+        pub struct $name {
+            pub id: String,
+            pub name: String,
+            #[serde(rename = "content")]
+            pub $field: $type,
+            pub ttl: String,
+            pub prio: String,
+            pub notes: Option<String>,
+        }
+    };
 }
+
+typed_record!(Ipv4Record, address, Ipv4Addr);
+typed_record!(Ipv6Record, address, Ipv6Addr);
 
 /// Authentication
 impl Client {
@@ -120,33 +134,55 @@ impl Client {
         })
     }
 
-    /// Tests authentication and returns the ipv4 address used to make the request.
+    /// Tests authentication and returns the ip address used to make the request.
     ///
-    /// api-ipv4.porkbun.com forces the use of ipv4, which is useful for updating the A record.
-    pub fn ping_ipv4(&self) -> Result<Option<Ipv4Addr>> {
-        const URL: &str = "https://api-ipv4.porkbun.com/api/json/v3/ping";
+    /// This will almost always be an ipv6 address. Use [ping_ipv4] to recieve an ipv4 address.
+    pub fn ping(&self) -> Result<Option<IpAddr>> {
+        self.ping_url("https://api.porkbun.com/api/json/v3/ping")
+    }
 
+    /// Tests authentication and returns the ipv4 address used to make the request.
+    pub fn ping_ipv4(&self) -> Result<Option<Ipv4Addr>> {
+        match self.ping_url("https://api-ipv4.porkbun.com/api/json/v3/ping") {
+            Ok(Some(IpAddr::V4(ip))) => Ok(Some(ip)),
+            Ok(Some(IpAddr::V6(ip))) => Err(Error::UnexpectedIpv6(ip)),
+            Ok(None) => Ok(None),
+            Err(msg) => Err(msg),
+        }
+    }
+
+    /// Tests authentication and returns the ipv6 address used to make the request.
+    pub fn ping_ipv6(&self) -> Result<Option<Ipv6Addr>> {
+        match self.ping_url("https://api.porkbun.com/api/json/v3/ping") {
+            Ok(Some(IpAddr::V4(ip))) => Err(Error::UnexpectedIpv4(ip)),
+            Ok(Some(IpAddr::V6(ip))) => Ok(Some(ip)),
+            Ok(None) => Ok(None),
+            Err(msg) => Err(msg),
+        }
+    }
+
+    fn ping_url(&self, url: &str) -> Result<Option<IpAddr>> {
         #[derive(Clone, Debug, serde::Deserialize)]
         struct PingResponse {
             status: Status,
             #[serde(default)]
             message: String,
             #[serde(rename = "yourIp")]
-            ip: Option<Ipv4Addr>,
+            ip: Option<IpAddr>,
         }
 
         #[cfg(feature = "tracing")]
-        tracing::info!("POST {URL}");
+        tracing::info!("POST {url}");
         let response = self
             .client
-            .post(URL)
+            .post(url)
             .body(self.key_file.clone())
             .send()?
             .text()?;
         #[cfg(feature = "tracing")]
         tracing::info!("response: {response}");
         let response: PingResponse = serde_json::from_str(&response)
-            .map_err(|error| Error::MalformedApi { error, response })?;
+            .map_err(|error| Error::MalformedApiSerde { error, response })?;
         #[cfg(feature = "tracing_debug")]
         tracing::debug!("parsed response: {response:?}");
         match response.status {
@@ -160,46 +196,63 @@ impl Client {
 
 /// Fetch records
 impl Client {
+    fn fetch_records_url<T: serde::de::DeserializeOwned>(&self, url: &str) -> Result<Vec<T>> {
+        #[cfg(feature = "tracing")]
+        tracing::info!("POST {url}");
+        let response = self
+            .client
+            .post(url)
+            .body(self.key_file.clone())
+            .send()?
+            .text()?;
+        #[cfg(feature = "tracing")]
+        tracing::info!("response: {response}");
+        let object = match serde_json::from_str::<serde_json::Value>(&response) {
+            Ok(object) => object,
+            Err(error) => return Err(Error::MalformedApiSerde { error, response }),
+        };
+        let error = Error::MalformedApi { response };
+        let Some(object) = object.as_object() else {
+            return Err(error);
+        };
+        let Some(status) = object
+            .get("status")
+            .and_then(serde_json::Value::as_str)
+            .and_then(|x| match x {
+                "SUCCESS" => Some(Status::Success),
+                "ERROR" => Some(Status::Error),
+                _ => None,
+            })
+        else {
+            return Err(error);
+        };
+        #[cfg(feature = "tracing_debug")]
+        tracing::debug!("parsed response: {response:?}");
+
+        match status {
+            Status::Success => Ok(serde_json::from_value(
+                object.get("records").ok_or(error)?.to_owned(),
+            )?),
+            Status::Error => Err(Error::Api {
+                message: object
+                    .get("message")
+                    .and_then(serde_json::Value::as_str)
+                    .ok_or(error)?
+                    .to_string(),
+            }),
+        }
+    }
+
     /// Fetches all DNS records for a given domain.
     ///
     /// These can be of any type and with any name.
     /// Consider using a more specific method if you're only interested in certain records,
     /// such as:
     /// - [Client::fetch_ipv4_records]
-    /// - [Client::fetch_ipv4_address_by_name]
+    /// - [Client::fetch_ipv6_records]
     pub fn fetch_records(&self, domain: &str) -> Result<Vec<DnsRecord>> {
-        #[derive(Clone, Debug, serde::Deserialize)]
-        struct FetchRecordsResponse {
-            status: Status,
-            #[serde(default)]
-            message: String,
-            #[serde(default)]
-            records: Vec<DnsRecord>,
-        }
-
         let url = format!("https://api.porkbun.com/api/json/v3/dns/retrieve/{domain}");
-
-        #[cfg(feature = "tracing")]
-        tracing::info!("POST {url}");
-        let response = self
-            .client
-            .post(&url)
-            .body(self.key_file.clone())
-            .send()?
-            .text()?;
-        #[cfg(feature = "tracing")]
-        tracing::info!("response: {response}");
-        let response: FetchRecordsResponse = serde_json::from_str(&response)
-            .map_err(|error| Error::MalformedApi { error, response })?;
-        #[cfg(feature = "tracing_debug")]
-        tracing::debug!("parsed response: {response:?}");
-
-        match response.status {
-            Status::Success => Ok(response.records),
-            Status::Error => Err(Error::Api {
-                message: response.message,
-            }),
-        }
+        self.fetch_records_url(&url)
     }
 
     /// Fetches all DNS A records for a given domain.
@@ -213,37 +266,21 @@ impl Client {
         if let Some(subdomain) = subdomain {
             url.push_str(subdomain);
         }
+        self.fetch_records_url(&url)
+    }
 
-        #[derive(Clone, Debug, serde::Deserialize)]
-        struct FetchIpv4RecordsResponse {
-            status: Status,
-            #[serde(default)]
-            message: String,
-            #[serde(default)]
-            records: Vec<Ipv4Record>,
+    /// Fetches all DNS AAAA records for a given domain.
+    pub fn fetch_ipv6_records(
+        &self,
+        domain: &str,
+        subdomain: Option<&str>,
+    ) -> Result<Vec<Ipv6Record>> {
+        let mut url =
+            format!("https://api.porkbun.com/api/json/v3/dns/retrieveByNameType/{domain}/AAAA/");
+        if let Some(subdomain) = subdomain {
+            url.push_str(subdomain);
         }
-
-        #[cfg(feature = "tracing")]
-        tracing::info!("POST {url}");
-        let response = self
-            .client
-            .post(&url)
-            .body(self.key_file.clone())
-            .send()?
-            .text()?;
-        #[cfg(feature = "tracing")]
-        tracing::info!("response: {response}");
-        let response: FetchIpv4RecordsResponse = serde_json::from_str(&response)
-            .map_err(|error| Error::MalformedApi { error, response })?;
-        #[cfg(feature = "tracing_debug")]
-        tracing::debug!("parsed response: {response:?}");
-
-        match response.status {
-            Status::Success => Ok(response.records),
-            Status::Error => Err(Error::Api {
-                message: response.message,
-            }),
-        }
+        self.fetch_records_url(&url)
     }
 }
 
@@ -300,7 +337,7 @@ impl Client {
         #[cfg(feature = "tracing")]
         tracing::info!("response: {response}");
         let response: Response = serde_json::from_str(&response)
-            .map_err(|error| Error::MalformedApi { error, response })?;
+            .map_err(|error| Error::MalformedApiSerde { error, response })?;
         #[cfg(feature = "tracing_debug")]
         tracing::debug!("parsed response: {response:?}");
         match response.status {
@@ -314,23 +351,7 @@ impl Client {
 
 /// Edit records
 impl Client {
-    pub fn edit_ipv4_address(
-        &self,
-        domain: &str,
-        subdomain: Option<&str>,
-        address: &Ipv4Addr,
-    ) -> Result<()> {
-        #[derive(Clone, Debug, serde::Serialize)]
-        struct EditDnsRecordBody<'a> {
-            #[serde(rename = "secretapikey")]
-            secret_api: &'a str,
-            #[serde(rename = "apikey")]
-            api: &'a str,
-            #[serde(rename = "type")]
-            ty: &'static str,
-            content: &'a Ipv4Addr,
-        }
-
+    fn edit_record_url<T: serde::Serialize>(&self, url: &str, ty: &str, content: &T) -> Result<()> {
         #[derive(Clone, Debug, serde::Deserialize)]
         struct EditDnsRecordResponse {
             status: Status,
@@ -338,28 +359,21 @@ impl Client {
             message: String,
         }
 
-        let mut url = format!("https://api.porkbun.com/api/json/v3/dns/editByNameType/{domain}/A/");
-        if let Some(subdomain) = subdomain {
-            url.push_str(subdomain);
-        }
+        let body = format!(
+            "{{\"secretapikey\":{},\"apikey\":{},\"type\":{},\"content\":{}}}",
+            self.secret_api_key,
+            self.api_key,
+            ty,
+            serde_json::to_string(content)?
+        );
 
         #[cfg(feature = "tracing")]
         tracing::info!("POST {url}");
-        let response = self
-            .client
-            .post(&url)
-            .body(serde_json::to_string(&EditDnsRecordBody {
-                secret_api: &self.secret_api_key,
-                api: &self.api_key,
-                ty: "A",
-                content: address,
-            })?)
-            .send()?
-            .text()?;
+        let response = self.client.post(url).body(body).send()?.text()?;
         #[cfg(feature = "tracing")]
         tracing::info!("response: {response}");
         let response: EditDnsRecordResponse = serde_json::from_str(&response)
-            .map_err(|error| Error::MalformedApi { error, response })?;
+            .map_err(|error| Error::MalformedApiSerde { error, response })?;
         #[cfg(feature = "tracing_debug")]
         tracing::debug!("parsed response: {response:?}");
         match response.status {
@@ -368,5 +382,31 @@ impl Client {
                 message: response.message,
             }),
         }
+    }
+
+    pub fn edit_ipv4_address(
+        &self,
+        domain: &str,
+        subdomain: Option<&str>,
+        address: &Ipv4Addr,
+    ) -> Result<()> {
+        let mut url = format!("https://api.porkbun.com/api/json/v3/dns/editByNameType/{domain}/A/");
+        if let Some(subdomain) = subdomain {
+            url.push_str(subdomain);
+        }
+        self.edit_record_url(&url, "A", address)
+    }
+
+    pub fn edit_ipv6_address(
+        &self,
+        domain: &str,
+        subdomain: Option<&str>,
+        address: &Ipv6Addr,
+    ) -> Result<()> {
+        let mut url = format!("https://api.porkbun.com/api/json/v3/dns/editByNameType/{domain}/A/");
+        if let Some(subdomain) = subdomain {
+            url.push_str(subdomain);
+        }
+        self.edit_record_url(&url, "A", address)
     }
 }
